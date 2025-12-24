feefecde31ff36edac446e188651587881c0fb17
Aman <azadaman4321@gmail.com>
2025-12-24 16:33:01 +0530

    day 33

---
+++ assignment8/3_const.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class ABC
+{
+    int x;
+    const int y;
+    public:
+        ABC(int a, int b):x(a),y(b)
+        {
+            // x=a;
+            // y=b;
+        }
+        void display()
+        {
+            cout<<x<<" "<<y<<endl;
+        }
+};
+int main()
+{
+    ABC a1(2,3);
+    a1.display();
+    return 0;
+}
