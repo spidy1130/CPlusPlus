641d642b5be24110bbd7471dec28bf6aef53f8a4
Aman <azadaman4321@gmail.com>
2025-12-25 16:57:12 +0530

    day 34

---
+++ assignment9(pointersInC++)/DMA.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class Complex
+{
+    int a,b;
+    public:
+        void setV(int x,int y)
+        { 
+            a=x;b=y;
+        }
+        void display()
+        {
+            cout<<"a "<<a<<"\nb "<<b<<endl;
+        }
+};
+void input(Complex *ptr)
+{
+    int *a=new int;
+    int *b=new int;
+    cout<<"enter two values: ";
+    cin>>*a>>*b;
+    ptr->setV(*a,*b);
+
+}
+int main()
+{
+    Complex *p= new Complex;
+    p->setV(12,21);
+    p->display();
+    Complex *ptr=new Complex();
+    input(ptr);
+    ptr->display();
+    return 0;
+}
